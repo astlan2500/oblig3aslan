276d33d66085fe3069833767a1325077c2f56c6c
astlan2500 <143513354+astlan2500@users.noreply.github.com>
2024-04-10 13:54:49 +0200

    u

---
+++ src/test/java/com/example/oblig3astlan/Oblig3astlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig3astlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3astlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
